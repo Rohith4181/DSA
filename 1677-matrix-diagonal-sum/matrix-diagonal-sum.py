7f067806ec89628a78fc409e5a1b84a3c9998bc0
Rohith4181 <rohithnaspuri4@gmail.com>
2026-01-22 11:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (37.64%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        row = len(mat)
+        col = len(mat[0])
+        sum = 0
+
+        for i in range(row):
+            sum += mat[i][i]
+        for i in range(row):
+            j = col-i-1
+            if(i!= j):
+                sum += mat[i][j]
+        return sum            
+        
